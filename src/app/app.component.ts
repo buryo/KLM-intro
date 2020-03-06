7118099ea33db8fa545dbf2ca869d8f42dbe25d3
Burak <b96.sen@gmail.com>
2020-03-06 18:24:56 +0100

    Solve merge conflicts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
-<<<<<<< Updated upstream
-import { Component, ViewEncapsulation } from '@angular/core';
-=======
 import { Component } from '@angular/core';
 import { slider } from './route-animations';
 import { RouterOutlet } from '@angular/router';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-<<<<<<< Updated upstream
-  encapsulation: ViewEncapsulation.None
-=======
   animations: [
     slider
   ]
->>>>>>> Stashed changes
 })
 export class AppComponent {
 
