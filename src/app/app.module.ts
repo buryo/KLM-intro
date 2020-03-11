cad0983e09f75ffafaa5ad5f5c27c24ef1b3554d
Burak <b96.sen@gmail.com>
2020-03-11 12:36:50 +0100

    Add test pages and module
    Adding example pages and module so we can replace these later with real pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { TestModule } from './test/test.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    RouterModule,
+    BrowserAnimationsModule,
+    TestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
