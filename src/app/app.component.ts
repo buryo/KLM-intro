4085f55acc8f089a643384bf1c6191acef4fa018
Rebien <k.rebien@gmail.com>
2020-03-12 19:29:35 +0100

    removed unnecessary dependcies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,42 @@ export class AppComponent {
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
+  
+  timeoutID: number;
+
+constructor(){}
+ 
+setup() {
+    document.addEventListener("mousemove", this.resetTimer, false);
+    document.addEventListener("mousedown", this.resetTimer, false);
+    document.addEventListener("keypress", this.resetTimer, false);
+    document.addEventListener("DOMMouseScroll", this.resetTimer, false);
+    document.addEventListener("mousewheel", this.resetTimer, false);
+    document.addEventListener("touchmove", this.resetTimer, false);
+    document.addEventListener("MSPointerMove", this.resetTimer, false);
+ 
+    thisstartTimer();
+}
+setup();
+ 
+ startTimer() {
+    // wait 2 seconds before calling goInactive
+    this.timeoutID = window.setTimeout(this.goInactive, 2000);
+}
+ 
+ resetTimer(e) {
+    window.clearTimeout(this.timeoutID);
+ 
+    this.goActive();
+}
+ 
+goInactive() {
+  console.log("I'm inActive");
+}
+ 
+goActive() {
+  console.log("I'm active");         
+    startTimer();
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { TestModule } from './test/test.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    TestModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
