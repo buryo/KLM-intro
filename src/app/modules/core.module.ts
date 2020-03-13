785a0d580ea650709f502e78a877b8d871cd7c36
Rebien <k.rebien@gmail.com>
2020-03-13 20:13:07 +0100

    imported modalComponent

--- src/app/modules/core.module.ts
+++ src/app/modules/core.module.ts
@@ -2,18 +2,27 @@ import { EcoSystemComponent } from './pages/eco-system/eco-system.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { NgbdModalComponent, ModalComponent } from '../shared/modal/modal.component';
 
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
-  declarations: [EcoSystemComponent],
+  declarations: [EcoSystemComponent,NgbdModalComponent, ModalComponent],
   imports: [
     AppRoutingModule,
     CommonModule,
-    FontAwesomeModule
-  ]
+    FontAwesomeModule,
+    BrowserModule,
+    ModalComponent,
+    NgbModule
+  ],
+  exports: [ModalComponent],
+  bootstrap: [ModalComponent],
+  entryComponents: [ModalComponent]
 })
 export class CoreModule { }
