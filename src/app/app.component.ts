934dda90cfcdbbe949465c72f9dcc3b6114e7a7e
Burak <b96.sen@gmail.com>
2020-03-13 13:07:41 +0100

    Replaced code to its own file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,34 +16,4 @@ export class AppComponent {
     );
   }
 
-  timer: any;
-
-  constructor() {}
-
-  //Seting up the listners to DOM if the user is active
-  setup() {
-    document.addEventListener("mousemove", ()=> {
-      this.resetTimer(this.timer)
-    });
-    // document.addEventListener("keypress", this.resetTimer);
-    this.timer = this.startTimer();
-  }
-
-  startTimer() {
-    // wait 2 seconds before calling checkInactive
-    return setTimeout(this.checkInactive, 2000);
-  }
-
-  //Reset the timer if the user is moving the mouse or pressing a key
-  resetTimer(timer) {
-    clearTimeout(timer);
-    this.timer = this.startTimer();
-  }
-
-  //If the user is inactive this function will excute
-  checkInactive() {
-    console.log("I'm inActive");
-  }
-
- 
 }
