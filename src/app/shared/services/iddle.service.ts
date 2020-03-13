8f5838edbe0500bd75fb1b5febfb3243be7323cb
Rebien <k.rebien@gmail.com>
2020-03-13 20:14:58 +0100

    Added autoRedirect function >WIP<

--- src/app/shared/services/iddle.service.ts
+++ src/app/shared/services/iddle.service.ts
@@ -1,16 +1,17 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class IddleService {
   // The Window timer.
   timer: NodeJS.Timeout;
-
   constructor(private router: Router) {
     this.setup();
     this.timer = this.startTimer();
+    this.autoRedirect();
   }
 
   //Seting up the listners to DOM if the user is active
@@ -20,15 +21,29 @@ export class IddleService {
       this.timer = this.startTimer();
     });
   }
+  
+  autoRedirect(){
+   return setTimeout(()=> {
+     console.log("autoRedirect");
+      if(this.timer){
+        this.router.navigate(['/'])
+      }
+    },10000)
+  }
 
-  // wait 2 seconds before calling checkInactive
+ // wait 20 seconds before calling checkInactive
   startTimer() {
     return setTimeout(() => {
-      if (confirm('Yo faggot, you there?')) {
+      console.log("activated");
+      
+      if (confirm('Hallo is iemand daar?')) {
         return false;
       } else {
         this.router.navigate(['/'])
       }
-    }, 20000);
+    }, 11000);
   }
+ 
+
+
 }
