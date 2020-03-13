88e7df2304674a5e83aca431e9d000e3c3e163cf
Burak <b96.sen@gmail.com>
2020-03-13 17:31:28 +0100

    Adding radicalComponent to the coreModule.

--- src/app/modules/core.module.ts
+++ src/app/modules/core.module.ts
@@ -1,15 +1,13 @@
 import { EcoSystemComponent } from './pages/eco-system/eco-system.component';
+import { RadicalInnovationComponent } from './pages/radical-innovation/radical-innovation.component';
+
 import { AppRoutingModule } from '../app-routing.module';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-
 import { NgModule } from '@angular/core';
 
-
-
 @NgModule({
-  declarations: [EcoSystemComponent],
+  declarations: [EcoSystemComponent, RadicalInnovationComponent],
   imports: [
     AppRoutingModule,
     CommonModule,
